20d66a9f619e5d9adbf144f6e56d03d917636cda
허지운 <cloudzioncloud9>
2023-03-12 13:17:59 +0900

    포트폴리오 커밋

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- todo -->
+    <!-- header -->
+    <Header :msg="'abcd'"></Header>
+    <!-- contents -->
+
+    <!-- footer -->
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue';
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+//    HelloWorld
+      Header
+  }
+}
+</script>
+
